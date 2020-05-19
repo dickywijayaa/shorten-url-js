69ff938e16328c6a7da01af9acb0ecbd43ca8d57
dickywijayaa <dicky.wijaya@ralali.com>
2020-05-20 00:17:14 +0700

    change code structure to promise based

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,53 +9,54 @@ exports.index = function(req, res) {
 }
 
 exports.postShorten = async function(req, res) {
-    try {
-        // validate input
-        if (!req.body.url) {
-            return response.badRequest(constants.REQUIRED_URL_INPUT, res);
-        }
-
-        if (!req.body.url.startsWith("http://") && !req.body.url.startsWith("https://")) {
-            return response.badRequest(constants.INVALID_URL_PREFIX, res);
-        }
-
-        if (!req.body.shortcode) {
-            return response.badRequest(constants.REQUIRED_SHORTCODE_INPUT, res)
-        }
-
-        let shortcodePattern = new RegExp("^[0-9a-zA-Z_]{6}$");
-        if (!shortcodePattern.test(req.body.shortcode)) {
-            return response.badRequest(constants.INVALID_REGEX_SHORTCODE_INPUT, res);
-        }
-
-        let payload = {
-            url: req.body.url,
-            code: req.body.shortcode
-        }
-
-        let result = await service.StoreShortenURL(payload)
-        if (result.status_code == constants.HTTP_UNPROCESSABLE_ENTITY) {
-            return response.unprocessableEntity(result.message, res)
-        }
-
-        return response.ok(result.data, res)
-        
-    } catch(e) {
-        console.log(e)
-        return response.internalServerError(res)
+    // validate input
+    if (!req.body.url) {
+        return response.badRequest(constants.REQUIRED_URL_INPUT, res);
     }
+
+    if (!req.body.url.startsWith("http://") && !req.body.url.startsWith("https://")) {
+        return response.badRequest(constants.INVALID_URL_PREFIX, res);
+    }
+
+    if (!req.body.shortcode) {
+        return response.badRequest(constants.REQUIRED_SHORTCODE_INPUT, res);
+    }
+
+    let shortcodePattern = new RegExp("^[0-9a-zA-Z_]{6}$");
+    if (!shortcodePattern.test(req.body.shortcode)) {
+        return response.badRequest(constants.INVALID_REGEX_SHORTCODE_INPUT, res);
+    }
+
+    let payload = {
+        url: req.body.url,
+        code: req.body.shortcode
+    }
+
+    return service.StoreShortenURL(payload)
+        .then(function(result) {
+            if (result.status_code == constants.HTTP_UNPROCESSABLE_ENTITY) {
+                return response.unprocessableEntity(result.message, res);
+            }
+    
+            return response.ok(result.data, res);
+        })
+        .catch(function(error) {
+            console.log(error);
+            return response.internalServerError(res);
+        });
 }
 
 exports.getURLFromShortcode = async function(req, res) {
-    try {
-        let result = await service.FetchURLByCode(req.params.shortcode)
-        if (result.status_code == constants.HTTP_UNPROCESSABLE_ENTITY) {
-            return response.unprocessableEntity(result.message, res)
-        }
+    return service.FetchURLByCode(req.params.shortcode)
+        .then(function(result) {
+            if (result.status_code == constants.HTTP_UNPROCESSABLE_ENTITY) {
+                return response.unprocessableEntity(result.message, res);
+            }
 
-        return res.redirect(result.data)
-    } catch(e) {
-        console.log(e)
-        return response.internalServerError(res)
-    }
+            return res.redirect(result.data);
+        })
+        .catch(function(error) {
+            console.log(error);
+            return response.internalServerError(res);
+        });
 }
--- services/service.js
+++ services/service.js
@@ -11,32 +11,43 @@ var result = {
 
 exports.StoreShortenURL = async function(payload) {
     // check code exists
-    let check = await repository.checkCodeExists(payload.code)
-
-    if (check && check[0].count > 0) {
-        result.status_code = constants.HTTP_UNPROCESSABLE_ENTITY;
-        result.message = constants.SHORTCODE_ALREADY_EXISTS_IN_DATABASE;
-        return result;
-    }
-
-    let insert = await repository.storeShortcode(payload)
-    payload.id = insert.insertId
-
-    result.status_code = constants.HTTP_STATUS_OK;
-    result.data = payload
-    return result;
+    return repository.checkCodeExists(payload.code)
+        .then(function(check) {
+            if (check && check[0].count > 0) {
+                result.status_code = constants.HTTP_UNPROCESSABLE_ENTITY;
+                result.message = constants.SHORTCODE_ALREADY_EXISTS_IN_DATABASE;
+                return result;
+            }
+        
+            return repository.storeShortcode(payload)
+                .then(function(insert) {
+                    payload.id = insert.insertId;
+        
+                    result.status_code = constants.HTTP_STATUS_OK;
+                    result.data = payload;
+                    return result;
+                });
+        })
+        .catch(function(error) {
+            console.log(error);
+            return result;
+        });
 }
 
 exports.FetchURLByCode = async function(code) {
-    let data = await repository.getURLFromCode(code)
+    return repository.getURLFromCode(code)
+        .then(function(data) {
+            if (data.length == 0) {
+                result.status_code = constants.HTTP_UNPROCESSABLE_ENTITY;
+                result.message = constants.SHORTCODE_NOT_EXISTS_IN_DATABASE;
+                return result;
+            }
 
-    if (data.length == 0) {
-        result.status_code = constants.HTTP_UNPROCESSABLE_ENTITY
-        result.message = constants.SHORTCODE_NOT_EXISTS_IN_DATABASE
-        return result
-    }
-
-    result.status_code = constants.HTTP_STATUS_OK
-    result.data = data[0].url
-    return result
+            result.status_code = constants.HTTP_STATUS_OK;
+            result.data = data[0].url;
+            return result;
+        }).catch(function(error) {
+            console.log(error);
+            return result;
+        });
 }
